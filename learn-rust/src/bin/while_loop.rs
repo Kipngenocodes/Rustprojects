96f5305f452d417eadc29683781468c7a771305a
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-12-17 12:03:16 -0800

    working with loops in rust

--- learn-rust/src/bin/while_loop.rs
+++ learn-rust/src/bin/while_loop.rs
@@ -9,12 +9,26 @@ fn main() {
     }
 
     println!("LIFTOFF!!!");
+
     // Stopping while loop with break
     let mut count = 1;
 
     while count <= 10 {
-        println!("Count: {}", count);
+        println!(" I am counting: {}", count);
         count += 1;
-        
+
+    // Skipping a value with continue
+        if count % 2 == 0 {
+            continue;
+        }
+        println!(" Odd number: {}", count);
+
+    // STOPPING the loop when count is greater than 15
+        if count > 15 {
+            break;
+        }
+
     }
 }
+
+
