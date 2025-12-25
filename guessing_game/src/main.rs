00f3704fbe2ce91b140b24221d9a197b68209e8a
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-12-25 07:47:09 -0800

    updates

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Pleasue input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line"); //rustc 1.0.0
+
+    println!("You guessed: {}", guess);
 }
