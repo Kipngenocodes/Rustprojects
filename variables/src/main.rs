b8b858352778d92168c8c153e782652b5f95d2cf
Kipngenocodes <kipngenoalex.c@gmail.com>
2026-01-04 09:12:20 -0800

    update

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,4 +4,21 @@ fn main() {
     // Variables are immutable by default, so we need to use 'mut' to make it mutable
     y = 34;
     println!("The value of y is: {}", y);
+
+    // Working on math operations
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
 }
